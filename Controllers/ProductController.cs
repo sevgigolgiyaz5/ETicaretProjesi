9af2c2b561af41aceb88cc2e2f3540d3e19be6de
sevgigolgiyaz5 <sevgigolgiyaz5@gmail.com>
2025-04-08 14:56:35 +0300

    Add files via upload

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ETicaretProjesi.Models;
+
+namespace ETicaretProjesi.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Laptop", Price = 15000, Description = "Oyun bilgisayarı", ImageUrl = "/images/laptop.jpg" },
+                new Product { Id = 2, Name = "Telefon", Price = 8000, Description = "Akıllı telefon", ImageUrl = "/images/telefon.jpg" }
+            };
+
+            return View(products);
+        }
+    }
+}
